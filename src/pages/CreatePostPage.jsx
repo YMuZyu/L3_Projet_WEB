73320e3eda9f76db7248a6fe247ec9398b28555f
Chenye <llchenye341202@gmail.com>
2026-06-02 20:08:31 +0200

    modif

--- src/pages/CreatePostPage.jsx
+++ src/pages/CreatePostPage.jsx
@@ -42,7 +42,7 @@ export default function CreatePostPage({ isConnected }) {
         }
 
         try {
-            const response = await fetch('/posts', {
+            const response = await fetch('http://localhost:10000/posts', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 credentials: 'include',
